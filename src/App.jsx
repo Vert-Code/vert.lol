4ca14c8f031cbd870a919330830930ce560fee90
Vert <hello@vert.codes>
2022-09-02 18:03:15 +1000

    Added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Link, Routes, Route, } from "react-router-dom";
 import Hello from './components/Hello'
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (
     <div className="App">
-      <Hello />
+    <Router>
+        <Routes>
+          <Route path="/" element={<Hello />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div className='text-white text-4xl flex align-middle text-bold'>Page Not Found</div>
+  )
+}
