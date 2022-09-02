c0937e6f783cd60595dafff5c164413dfdf43ef4
Vert <hello@vert.codes>
2022-09-02 18:30:10 +1000

    Added 404 Assets and Text

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <div className='text-white text-4xl flex align-middle text-bold'>Page Not Found</div>
+    <div class="min-w-screen min-h-screen flex items-center p-5 lg:p-20 overflow-hidden relative">
+    <div class="flex-1 min-h-full min-w-full rounded-3xlshadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left">
+        <div class="w-full md:w-1/2">
+            <div class="mb-10 md:mb-20 text-gray-600 font-light">
+                <h1 class="font-black text-3xl lg:text-5xl text-white mb-10">You seem to be lost!</h1>
+            </div>
+        </div>
+    </div>
+  </div>
   )
 }
+ 
